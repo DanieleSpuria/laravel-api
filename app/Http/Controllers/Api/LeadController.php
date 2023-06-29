e6d26dade9eeab3f0cf5c3ad34389608b1ce52ee
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-29 15:31:04 +0200

    save $data

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
+use App\Models\Lead;
 
 class LeadController extends Controller
 {
-  public function store() {
-
+  public function store(Request $request) {
+    $data = $request->all();
+    $new_lead = Lead::create($data);
+    return response()->json($data);
   }
 }
