5a7399bf41cfd4cae6ebfac55b9dd416b6abd333
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-07-03 23:40:00 +0200

    send mail ok

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\newContact;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 use App\Models\Lead;
@@ -11,7 +13,30 @@ class LeadController extends Controller
 {
   public function store(Request $request) {
     $data = $request->all();
+
+    $validator = Validator::make($data,
+      [
+        'name' => 'required',
+        'email' => 'required|email',
+        'text' => 'required',
+      ],
+      [
+        'name.required' => 'Campo obbligatorio',
+        'email.required' => 'Campo obbligatorio',
+        'email.email' => 'Indirizzo non corretto',
+        'text.required' => 'Campo obbligatorio',
+      ]);
+
+    if ($validator->fails()) {
+      $success = false;
+      $errors = $validator->errors();
+      return response()->json(compact('success', 'errors'));
+    }
+
     $new_lead = Lead::create($data);
-    return response()->json($data);
+    Mail::to('hello@example.com')->send(new newContact($new_lead));
+
+    $success = true;
+    return response()->json('success');
   }
 }
---
+++ resources/views/mail/new-contact.blade.php
@@ -0,0 +1,3 @@
+<span>Name: {{ $lead->name }}</span> <br>
+<span>Email: {{ $lead->email }}</span> <br>
+<span>Text: {{ $lead->text }}</span>
